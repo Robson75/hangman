b5916f4fdcc75614bc96ff40a8fdb61bdff42db8
Robson75 <rsamuelsson@gmail.com>
2020-02-04 21:30:40 +0100

    Start implementing button functionality

--- androidstudio/app/src/main/java/com/samdide/android/hangman/Hangman.kt
+++ androidstudio/app/src/main/java/com/samdide/android/hangman/Hangman.kt
@@ -4,12 +4,42 @@ import android.util.Log
 
 private const val TAG = "Hangman"
 
-class Hangman(var word: String = "test") {
+class Hangman(var word: String = "TEST") {
+    private val replaceCharacter = '_'
+    private var display_word: String
+    private var found_characters = mutableSetOf<Char>()
+    init{
+        display_word = init_display(word)
+    }
 
     public fun guess(guess_char: Char): String {
-        val replaceCharacter: Char = '_'
-        word = word.replace(guess_char, replaceCharacter, true)
-        Log.d(TAG, "word after guess: " + word)
-        return word
+        display_word = ""
+        // If guessed character in word add it to the set of found characters
+        if(guess_char in word){
+            found_characters.add(guess_char)
+        }
+        for(char in word){
+            if (char in found_characters){
+                display_word += char + " "
+            }else{
+                display_word += replaceCharacter + " "
+            }
+
+        }
+        Log.d(TAG, "word after guess: " + display_word)
+        return display_word
     }
+
+    private fun init_display(word: String):String{
+        display_word = ""
+        for (char in word){
+            display_word += "_ "
+        }
+        return display_word
+    }
+
+    public fun get_display_word():String{
+        return display_word
+    }
+
 }
--- androidstudio/app/src/main/java/com/samdide/android/hangman/MainActivity.kt
+++ androidstudio/app/src/main/java/com/samdide/android/hangman/MainActivity.kt
@@ -1,13 +1,15 @@
 package com.samdide.android.hangman
 
 import android.graphics.drawable.AnimationDrawable
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.DisplayMetrics
+import android.view.View
+import android.widget.Button
 import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import kotlin.math.roundToInt
 
-import android.util.Log
 
 private const val TAG = "MainActivity"
 
@@ -16,6 +18,10 @@ private const val IMAGE_ASPECT_RATIO = 2.1 / 1.5
 
 class MainActivity : AppCompatActivity() {
     lateinit var escapeAnimation: AnimationDrawable
+
+    // test with word "hello"
+    var word = "HELLO"
+    var hangman = Hangman(word)
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -33,9 +39,23 @@ class MainActivity : AppCompatActivity() {
         escapeImage.setOnClickListener{
             escapeAnimation.start() }
 
-        var word = "hello"
-        var hangman = Hangman(word)
-        hangman.guess('l')
-        hangman.guess('h')
+        val word_TV = findViewById<TextView>(R.id.word)
+        word_TV.text = hangman.get_display_word()
+        //hangman.guess('l')
+        //hangman.guess('h')
     }
+
+    fun character_click(view: View) {
+        val b: Button = view as Button
+        val guess_char: Char = b.text.toString()[0]
+
+        var displayWord:String = hangman.guess(guess_char)
+        setDisplayText(displayWord)
+    }
+
+    fun setDisplayText(word: String){
+        val word_TV = findViewById<TextView>(R.id.word)
+        word_TV.text = word
+    }
+
 }
