a7d2010e47b3b2fa5868ddc22662a2fd8fda5035
Robson75 <rsamuelsson@gmail.com>
2020-02-08 10:57:07 +0100

    Add graphics folder

--- androidstudio/app/src/main/java/com/samdide/android/hangman/MainActivity.kt
+++ androidstudio/app/src/main/java/com/samdide/android/hangman/MainActivity.kt
@@ -1,17 +1,21 @@
 package com.samdide.android.hangman
 
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
 import android.graphics.Color
 import android.graphics.PorterDuff
 import android.graphics.drawable.AnimationDrawable
 import android.os.Bundle
 import android.os.Handler
 import android.util.DisplayMetrics
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.*
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlin.math.roundToInt
 
 
@@ -25,6 +29,7 @@ class MainActivity : AppCompatActivity() {
     lateinit var escapeAnimation: AnimationDrawable
     lateinit var word: String
     lateinit var hangman: Hangman
+    lateinit var imageStar: ImageView
     var wordNr = 0
 
     // All words in uppercase
@@ -48,6 +53,12 @@ class MainActivity : AppCompatActivity() {
         setDisplayText(displayWord)
         var guesses:Int = hangman.getGuesses()
         setImage(guesses)
+
+        //handle win condition
+        Log.d(TAG, "display word: " + displayWord + " word: " + word)
+        if (displayWord.replace(" ", "").equals(word)){
+            runWinAnimation()
+        }
     }
 
     private fun View.disable() {
@@ -90,12 +101,15 @@ class MainActivity : AppCompatActivity() {
 
     }
     private fun initGame(wordNr: Int){
-        word = wordList[wordNr]
+        word = wordList[wordNr].toUpperCase()
         hangman = Hangman(word)
+
         setContentView(R.layout.activity_main)
         val displayMetrics = DisplayMetrics()
         windowManager.defaultDisplay.getMetrics(displayMetrics)
         val width = displayMetrics.widthPixels
+        imageStar = findViewById(R.id.star)
+        imageStar.visibility = View.GONE
         imageHolder = findViewById(R.id.hangManView)
         imageHolder.layoutParams.width = width;
         imageHolder.layoutParams.height = (width * IMAGE_ASPECT_RATIO).roundToInt();
@@ -104,8 +118,40 @@ class MainActivity : AppCompatActivity() {
         val word_TV = findViewById<TextView>(R.id.word)
         word_TV.text = hangman.get_display_word()
 
-        // set hangman graphic back
+        // set hangman graphic to first frame
         setImage(0)
+    }
+    private fun runWinAnimation(){
+        Log.d(TAG, "run win animation")
+        imageStar.apply {
+            // Set the star view to 0% opacity but visible, so that it is visible
+            // (but fully transparent) during the animation.
+            alpha = 0f
+            visibility = View.VISIBLE
 
+            // Animate the star view to 100% opacity, and clear any animation
+            // listener set on the view.
+            animate()
+                .alpha(1f)
+                .setDuration(500)
+                .setListener(null)
+                .setListener(object : AnimatorListenerAdapter() {
+                    override fun onAnimationEnd(animation: Animator) {
+                        wordNr ++
+                        if (wordNr >= wordList.size) wordNr = 0
+                        initGame(wordNr)
+                    }
+                })
+        }
+        // Animate the hangman view to 0% opacity. After the animation ends,
+        // set its visibility to GONE as an optimization step (it won't
+        // participate in layout passes, etc.)
+        hangManView.animate()
+            .alpha(0f)
+            .setDuration(500)
+    }
+    private fun fadeInNewGame(){
+        imageStar.animate()
+            .alpha(0f)
     }
 }
